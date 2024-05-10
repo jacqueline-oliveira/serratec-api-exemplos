3066ed90586e83816fe50f77d176abeb1fae2786
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2024-05-09 21:24:25 -0300

    aula 04 continuação

--- aula04/src/main/java/br/org/serratec/aula04/FilmeController.java
+++ aula04/src/main/java/br/org/serratec/aula04/FilmeController.java
@@ -1,12 +1,16 @@
 package br.org.serratec.aula04;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,24 +23,44 @@ public class FilmeController {
 	private FilmeRepository repositorio;
 	
 	@GetMapping
-	public List<Filme> obterTodosOsFilmes() {
-		return repositorio.findAll();
+	public ResponseEntity<List<Filme>> obterTodosOsFilmes() {
+		return ResponseEntity.ok(repositorio.findAll());
 	}
 
 	@PostMapping
-	public Filme cadastrarFilme(@RequestBody Filme filme) {
+	public ResponseEntity<Filme> cadastrarFilme(@RequestBody Filme filme) {
 		repositorio.save(filme);
-		return filme;
+		return new ResponseEntity<Filme>(filme, HttpStatus.CREATED);
+		
+		//URI endpoint = uriBuilder.path("/filmes/{id}").buildAndExpand(filme.getId()).toUri();
+	    //return ResponseEntity.created(endpoint).body(filme);
 	}
 	
 	@GetMapping("/{id}")
-	public Filme obterPorId(@PathVariable Long id) {
-		return repositorio.findById(id).get();
+	public ResponseEntity<Filme> obterPorId(@PathVariable Long id) {
+		Optional<Filme> filme = repositorio.findById(id);
+		
+		if (filme.isPresent()) {
+			return ResponseEntity.ok(filme.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 	
 	@DeleteMapping("/{id}")
-	public void excluirPorId(@PathVariable Long id) {
+	public ResponseEntity<Void> excluirPorId(@PathVariable Long id) {
 		repositorio.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Filme> alterarDadosFilme(@PathVariable Long id, @RequestBody Filme filmeAlterado) {
+		if (repositorio.existsById(id)) {
+			filmeAlterado.setId(id);
+			repositorio.save(filmeAlterado);
+			return ResponseEntity.ok(filmeAlterado);
+		}
+		
+		return ResponseEntity.badRequest().build();
 	}
 	
 	
